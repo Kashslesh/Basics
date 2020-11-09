c838cc8b90f2cdba6353f05f1f81b37ad294035a
Kashslesh <Kash174rus@gmail.com>
2020-11-09 10:11:13 +0100

    JS Challenge

--- terminal-js.js
+++ terminal-js.js
@@ -8,62 +8,81 @@
 
 
 
-
-
 //Déclarer deux variables avec comme valeur 4 et 5
 //Créez une autre variable contenant l'addition des deux et l'afficher.
+// let a = 4;
+// let b = 5;
+// console.log(a+b);
+
 //TODO
 
 
 //Afficher la chaine de caractère suivante en majuscule
-const team = 'avengers';
+//const part = 'avengers';
+//const partM = part.toUpperCase();
+//console.log(partM);
+
+//Afficher la première lettre de la chaine de caractère suivante
+// const team = 'avengers';
+// const teamM = team[0];
+// console.log(teamM);
+// //TODO
+
+
+//afficher la chaine de caractère suivante excepté la première lettre.
+// let team = 'avengers';
+// let team1 = team[1];
+// let team2 = team[2];
+// let team3 = team[3];
+// let team4 = team[4];
+// let team5 = team[5];
+// let team6 = team[6];
+// let team7 = team[7];
+// console.log(team1+team2+team3+team4+team5+team6+team7);
 //TODO
 
-//afficher la première lettre de la chaine de caractère suivante
-const team = 'avengers';
-//TODO
-
-//afficher la chaine de caractère suivante exepté la première lettre.
-const team = 'avengers';
-//TODO
 
 //Afficher les deux chaines suivantes dans une seule chaine de caractère (concaténation)
-const firstName = 'Tony';
-const lastName = 'Stark';
+// let firstName = 'Tony';
+// let lastName = 'Stark';
+// console.log (firstName +' '+ lastName);
 //TODO
-
 
 //Afficher la chaine de caractère suivante avec seulement la première lettre en majuscule.
-const team = 'aveNgers';
-//=> Avengers
-//TODO
-
-//Pause ! Va boire un café ! Tu le mérites...
+// let team = 'aveNgers';
+// let teamMn = team[0].toUpperCase()+ "vengers";
+// console.log(teamMn);
 
 //Les tableaux
 //Déclarez un tableau avec 5 villes
 //Afficher la ville à l'indice 3
 //Ajouter une ville au tableau
 //Supprimer la ville à l'indice 2
-const villes = [];
-//TODO
+// const villes = ["Paris", "Rennes", "Avignon", "Nice", "Lyon"];
+// console.log (villes[2]);
+// villes[5] = "Marseille"
+// console.log(villes);
+// villes.splice(2, 1);
+// console.log(villes);
 
-
-//Les boucles
-//
 //Comme tu le sais sans doute, pour parcourir un tableau,
 // il faut bBoucler sur ce tableau. Parcours le tableau suivant afin de monter chaque
 //éléments au carré et afficher le tableau.
 
-const array1 = [2, 4, 8];
-//TODO
+// const array1 = [2, 4, 8];
+// array1 .forEach ((element) => {
+//    console.log(element*2)
 
+// });
 
 // Le tableau suivant indique les notes d'une promo lointaine !
 // Calculer la moyenne de cette promo et afficher le resultat
-const promo = [12, 13, 17, 3, 14, 18];
-
-
+// const promo = [12, 13, 17, 3, 14, 18];
+// sum= 0;
+// promo .forEach ((element) => {
+    // sum = sum + element}
+// );
+// console.log(sum/6);
 
 //les conditions
 
@@ -71,99 +90,27 @@ const promo = [12, 13, 17, 3, 14, 18];
 //En utilisant les conditions 'if' et 'else' affichez :
 //peut voter si 'age' est supérieur ou égal à 18
 //ne peut pas voter si 'age' est inférieur à 18
-
+// const age = 30;
+// if (age >= 18) {
+//     console.log("vous pouvez voter");
+// }
+//  else {
+//     console.log("vous ne pouvez pas voter");
+// }
 
 // Afficher la date d'aujourd'hui avec javascript.
 // En utilisant les conditions 'if' et 'else' affichez :
 // "bonjour" si il est en 5h du matin et 17h
 // et "bonsoir" s'il est entre 17h et 5h du matin
+// const date = new Date();
+// const time = date.getHours();
+// console.log(date);
+// if (5 < time < 17){
+//     console.log("Bonjour");
+// } else {
+//     console.log("Bonsoir");
+// }
 
 
-//Va reboir un café ! Bravo
-//Et pense à commit ton projet
-
-
-//----------------- Fonctions -------------------
-
-//Créez deux variables initialisées à 3 et 4.
-//Créez une fonction qui retourne l'addition de ces deux valeurs.
-const add = (num1, num2) => {
+//=> Avengers
 //TODO
-};
-
-
-
-
-
-//Créez une fonction qui prend en paramètre la valeur i. La fonction doit
-//retourner la valeur i au carré et l'afficher.
-
-const square = (number) => {
-  //TODO
-};
-
-
-
-// En utilisant la methode 'map', mettre la chaine de
-// caractère en majuscule.
-const beatles = ["paul", "john", "ringo", "george"];
-
-//Créez un tableau nommé tab dont le premier
-//élément est -2, le deuxième 3 et le troisième 4
-//Créez une fonction qui soustrait chaque éléments du tableau au suivant.
-
-const soustrait = (tableau) => {
- //TODO
-};
-
-
-
-
-
-
-//Créez une fonction qui renvoie la somme de chaque éléments du
-//tableau si celui-ci est supérieur à 0.
-const array2 = [-2, 4, -5, 3, 6];
-const addPositiveNumber = (tableau) => {
- //TODO
-};
-
-
-
-
-//Créez une fonction qui prend un parametre 'r' et qui
-//renvoie le périmetre d'un cercle.
-const périmetreCercle = (r) => {
-  //TODO
-};
-
-//Créez une fonction qui met la premiere lettre d'un mot en majuscule.
-//Le reste du mot doit être en minuscule.
-//Le mot est "SimPloN"
-const capitalize = (word) => {
-  //TODO
-};
-
-
-
-//Odd or Even
-//Créez un fonction qui prend un nombre en parametre et qui renvoie :
-//paire si le nombre est pair
-//impaire si le nombre est impair
-//Tips : jetez un coup d'oeil à l'opérateur '%' dans la doc.
-
-
-
-//Mot inversé
-//créez une fonction qui prend en paramètre une chaine de caractère.
-//Cette fonction renvoie la chaine de caractère inversée.
-//ex: reverseWord('simplon') ==> 'nolpmis'
-//Aide : join(), split(), reverse()
-const reverseWord = (word) => {
-  //TODO
-};
-
-
-//créez une fonction qui renvoie la somme de tableau
-const nombres = ["3", "5", "6", "2"];
-
